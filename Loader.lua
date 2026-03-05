740420a7430e302c5e6f8b71de1e6a79c6febe84
icewinrage <nurelm.1000@gmail.com>
2026-03-05 16:02:22 +0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,33 +1,25 @@
+-- Data Hub Loader
 repeat task.wait() until game.IsLoaded
 repeat task.wait() until game.GameId ~= 0
 
-if Parvus and Parvus.Loaded then
-    Parvus.Utilities.UI:Push({
-        Title = "Parvus Hub",
+if DataHub and DataHub.Loaded then
+    DataHub.Utilities.UI:Push({
+        Title = "Data Hub",
         Description = "Script already running!",
         Duration = 5
     }) return
 end
-
---[[if Parvus and (Parvus.Game and not Parvus.Loaded) then
-    Parvus.Utilities.UI:Push({
-        Title = "Parvus Hub",
-        Description = "Something went wrong!",
-        Duration = 5
-    }) return
-end]]
 
 local PlayerService = game:GetService("Players")
 repeat task.wait() until PlayerService.LocalPlayer
 local LocalPlayer = PlayerService.LocalPlayer
 
 local Branch, NotificationTime, IsLocal = ...
---local ClearTeleportQueue = clear_teleport_queue
 local QueueOnTeleport = queue_on_teleport
 
 local function GetFile(File)
-    return IsLocal and readfile("Parvus/" .. File)
-    or game:HttpGet(("%s%s"):format(Parvus.Source, File))
+    return IsLocal and readfile("DataHub/" .. File)
+    or game:HttpGet(("%s%s"):format(DataHub.Source, File))
 end
 
 local function LoadScript(Script)
@@ -35,17 +27,16 @@ local function LoadScript(Script)
 end
 
 local function GetGameInfo()
-    for Id, Info in pairs(Parvus.Games) do
+    for Id, Info in pairs(DataHub.Games) do
         if tostring(game.GameId) == Id then
             return Info
         end
     end
-
-    return Parvus.Games.Universal
+    return DataHub.Games.Universal
 end
 
-getgenv().Parvus = {
-    Source = "https://raw.githubusercontent.com/AlexR32/Parvus/" .. Branch .. "/",
+getgenv().DataHub = {
+    Source = "https://raw.githubusercontent.com/icewinrage/Data/" .. Branch .. "/",
 
     Games = {
         ["Universal" ] = { Name = "Universal",                  Script = "Universal"  },
@@ -61,30 +52,29 @@ getgenv().Parvus = {
     }
 }
 
-Parvus.Utilities = LoadScript("Utilities/Main")
-Parvus.Utilities.UI = LoadScript("Utilities/UI")
-Parvus.Utilities.Physics = LoadScript("Utilities/Physics")
-Parvus.Utilities.Drawing = LoadScript("Utilities/Drawing")
+DataHub.Utilities = LoadScript("Utilities/Main")
+DataHub.Utilities.UI = LoadScript("Utilities/UI")
+DataHub.Utilities.Physics = LoadScript("Utilities/Physics")
+DataHub.Utilities.Drawing = LoadScript("Utilities/Drawing")
 
-Parvus.Cursor = GetFile("Utilities/ArrowCursor.png")
-Parvus.Loadstring = GetFile("Utilities/Loadstring")
-Parvus.Loadstring = Parvus.Loadstring:format(
-    Parvus.Source, Branch, NotificationTime, tostring(IsLocal)
+DataHub.Cursor = GetFile("Utilities/ArrowCursor.png")
+DataHub.Loadstring = GetFile("Utilities/Loadstring")
+DataHub.Loadstring = DataHub.Loadstring:format(
+    DataHub.Source, Branch, NotificationTime, tostring(IsLocal)
 )
 
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.InProgress then
-        --ClearTeleportQueue()
-        QueueOnTeleport(Parvus.Loadstring)
+        QueueOnTeleport(DataHub.Loadstring)
     end
 end)
 
-Parvus.Game = GetGameInfo()
-LoadScript(Parvus.Game.Script)
-Parvus.Loaded = true
+DataHub.Game = GetGameInfo()
+LoadScript(DataHub.Game.Script)
+DataHub.Loaded = true
 
-Parvus.Utilities.UI:Push({
-    Title = "Parvus Hub",
-    Description = Parvus.Game.Name .. " loaded!\n\nThis script is open sourced\nIf you have paid for this script\nOr had to go thru ads\nYou have been scammed.",
+DataHub.Utilities.UI:Push({
+    Title = "Data Hub",
+    Description = DataHub.Game.Name .. " loaded!\n\nThis script is open sourced\nIf you have paid for this script\nOr had to go thru ads\nYou have been scammed.",
     Duration = NotificationTime
 })
